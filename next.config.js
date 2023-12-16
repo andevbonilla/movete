419b7fa639b1ef26f91553682ad1c57900bd69cf
Andres Bonilla <andresbonilla1256@gmail.com>
2023-12-16 16:57:02 -0500

    basic translate languages structure espanol and english made

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withNextIntl = require('next-intl/plugin')();
+ 
+module.exports = withNextIntl({
+  // Other Next.js configuration ...
+});
 
-module.exports = nextConfig
