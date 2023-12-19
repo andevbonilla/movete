9f04d485cbc7a0e2aee06e7a0cfbf71f4aced77d
Andres Bonilla <andresbonilla1256@gmail.com>
2023-12-19 12:25:10 -0500

    preview profile and change the links of landing page

--- src/app/[locale]/auth/register/customize-profile/page.tsx
+++ src/app/[locale]/auth/register/customize-profile/page.tsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import { useRouter } from 'next/navigation';
 import { Dela_Gothic_One } from '@next/font/google';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const delaGothic = Dela_Gothic_One({
     weight: ["400"], // bold de la fuente
@@ -19,26 +21,54 @@ const CustomizePage = () => {
     };
 
     return (
-        <div className='h-screen flex flex-col items-center p-[10%] overflow-y-scroll'>
-            <h1 className={`text-white ${delaGothic.className} text-2xl mt-[3rem]`}>Customization</h1>
-            <p className='text-white text-lg text-center mt-4 mb-[3rem]'>All right, now let&apos;s customize your profile.</p>
+        <>
 
-            <div className='w-full flex flex-col items-start mb-10'>
-                <p className='text-white mb-2 text-lg font-bold'>Tell us your name:</p>
-                <input type="text" placeholder='Your Name' className='py-3 px-5 text-black w-full rounded' />
-            </div>
+            <div className='h-screen flex flex-col items-center p-[10%] overflow-y-scroll relative'>
 
-            <div className='w-full flex flex-col items-start mb-10'>
-                <p className='text-white mb-2 text-lg font-bold'>Select the color palette:</p>
-                <div className='flex justify-center items-center w-full mt-4'>
-                    <div className='rounded-full mr-8 w-8 h-8 bg-red-400'></div>
-                    <div className='rounded-full mr-8 w-8 h-8 bg-yellow-400'></div>
-                    <div className='rounded-full w-8 h-8 bg-green-400'></div>
+                <button type='button' className='fixed mx-[15%] bottom-0 left-0 right-0 mb-8 bg-[#00FF8F] text-white font-bold text-xl rounded-full py-3 px-6'>
+                    <FontAwesomeIcon icon={faEye} className='mr-3' />
+                    Preview Profile
+                </button>
+
+                <h1 className={`text-white ${delaGothic.className} text-2xl mt-[3rem]`}>Customization</h1>
+                <p className='text-white text-lg text-center mt-4 mb-[3rem]'>All right, now let&apos;s customize your profile.</p>
+
+                <div className='w-full flex flex-col items-start mb-10'>
+                    <p className='text-white mb-2 text-lg font-bold'>Tell us your name:</p>
+                    <input type="text" placeholder='Your Name' className='py-3 px-5 text-black w-full rounded' />
                 </div>
-            </div>
 
-            <button onClick={continueUserInfo} type='button' className={`bg-[#00FF8F] text-white mt-4 py-3 px-5 text-center w-full rounded font-extrabold text-lg`}>Continue</button>
-        </div>
+                <div className='w-full flex flex-col items-start mb-10'>
+                    <p className='text-white mb-2 text-lg font-bold'>Select the color palette:</p>
+                    <div className='flex justify-center items-center w-full mt-4'>
+
+                        <div className='flex items-center rotate-45 border-2 border-white rounded-full'>
+                            <div className='bg-[#ffffff] w-[1rem] h-[2rem] rounded-l-full'></div>
+                            <div className='bg-[#0092F7] w-[1rem] h-[2rem] rounded-r-full'></div>
+                        </div>
+
+                        <div className='flex items-center rotate-45 ml-10 border-2 border-white rounded-full'>
+                            <div className='bg-[#001429] w-[1rem] h-[2rem] rounded-l-full'></div>
+                            <div className='bg-[#00FF8F] w-[1rem] h-[2rem] rounded-r-full'></div>
+                        </div>
+
+                        <div className='flex items-center rotate-45 ml-10 border-2 border-white rounded-full'>
+                            <div className='bg-[#ffffff] w-[1rem] h-[2rem] rounded-l-full'></div>
+                            <div className='bg-[#FF0303] w-[1rem] h-[2rem] rounded-r-full'></div>
+                        </div>
+
+                        <div className='flex items-center rotate-45 ml-10 border-2 border-white rounded-full'>
+                            <div className='bg-[#ffffff] w-[1rem] h-[2rem] rounded-l-full'></div>
+                            <div className='bg-[#7203FF] w-[1rem] h-[2rem] rounded-r-full'></div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <button onClick={continueUserInfo} type='button' className={`bg-[#00FF8F] text-white mt-4 py-3 px-5 text-center w-full rounded font-extrabold text-lg`}>Continue</button>
+
+            </div>
+        </>
     )
 }
 
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,6 +4,7 @@ import { Dela_Gothic_One } from "@next/font/google";
 import { Navbar } from '@/components/Navbar';
 import { Footer } from '@/components/Footer';
 import { ExampleProfiles } from '@/components/ExampleProfiles';
+import Link from 'next/link';
 
 const delaGothic = Dela_Gothic_One({
     weight: ["400"], // bold de la fuente
@@ -37,12 +38,15 @@ export default function Index() {
                     <p className={`text-xl mt-6 text-opacity-50`}>
                         {t('sub-presentation')}
                     </p>
-                    <div>
+                    <div className='w-full flex flex-col'>
                         <div className='w-full flex items-center justify-start bg-white py-3 px-4 text-black rounded mt-4 text-lg'>
                             <span className='font-bold'>fitalgo.com/</span>
                             <input className='w-full' type="text" placeholder={`${t('yourname-placeholder')}`} />
                         </div>
-                        <button type='button' className={`bg-[#00FF8F] mt-4 py-3 px-5 text-center w-full rounded font-extrabold text-lg`}>{t('get-url')}</button>
+                        <Link href={'/en/auth/register/link'}
+                            className={`bg-[#00FF8F] mt-4 py-3 px-5 text-center w-full rounded font-extrabold text-lg`}>
+                            {t('get-url')}
+                        </Link>
                     </div>
                 </section>
 
@@ -50,20 +54,22 @@ export default function Index() {
                     <ExampleProfiles />
                 </section>
 
-                <section className='p-[10%]'>
+                <section className='p-[10%] w-full flex flex-col'>
                     <h2 className={`text-2xl ${delaGothic.className}`}>
                         Create and customize your bio fitness with fitalgo in a short time
                     </h2>
                     <p className='mt-3'>Show everyone how much weight you lift, add your links like tiktok, instagram, website, etc. connect with people with the same interest as you and much more.</p>
-                    <button type='button' className={`bg-[#00FF8F] mt-4 py-3 px-5 text-center w-full rounded font-extrabold text-lg`}>Get starter</button>
+                    <Link href={'/en/auth/register/link'}
+                        className={`bg-[#00FF8F] mt-4 py-3 px-5 text-center w-full rounded font-extrabold text-lg`}>Get starter</Link>
                 </section>
 
-                <section className='p-[10%]'>
+                <section className='p-[10%] w-full flex flex-col'>
                     <h2 className={`text-2xl ${delaGothic.className}`}>
                         The fitness child between linktree and VSCO
                     </h2>
                     <p className='mt-3'>Upload photos of your lifestyle, show what you listen to most when you exercise, add custom buttons like: train with me and meet people with the same interests and much more.</p>
-                    <button type='button' className={`bg-[#00FF8F] mt-4 py-3 px-5 text-center w-full rounded font-extrabold text-lg`}>Get starter</button>
+                    <Link href={'/en/auth/register/link'}
+                        className={`bg-[#00FF8F] mt-4 py-3 px-5 text-center w-full rounded font-extrabold text-lg`}>Get starter</Link>
                 </section>
 
 
