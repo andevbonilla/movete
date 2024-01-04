4bc6519de9813dd8b6a9aea5352fe4022a0e5df9
Andres Bonilla <andresbonilla1256@gmail.com>
2024-01-04 16:41:34 -0500

    update design of some cards

--- src/components/sport-cards/AthelticCard.tsx
+++ src/components/sport-cards/AthelticCard.tsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import { NoTeamPhoto } from '../no-file-components/NoTeamPhoto';
-
 import { Dela_Gothic_One } from "@next/font/google";
 
 const delaGothic = Dela_Gothic_One({
@@ -15,47 +13,35 @@ export const AthleticCard = () => {
 
             <div className='flex flex-col justify-between w-full items-center'>
 
-                <div className='mb-5'>
-                    <NoTeamPhoto />
+                <div className='flex w-full justify-between mt-4'>
+                    <div className='mr-3'>
+                        <h3 className='font-bold'>Medals:</h3>
+                        <p className={`${delaGothic.className}`}>16</p>
+                    </div>
+                    <div>
+                        <h3 className='font-bold'>Marathons:</h3>
+                        <p className={`${delaGothic.className}`}>7</p>
+                    </div>
                 </div>
 
-                <div>
-
-                    <div>
-                        <h2 className={`${delaGothic.className} font-bold text-xl`}>Real Valencianos</h2>
+                <div className='flex w-full justify-between mt-4'>
+                    <div className='mr-3'>
+                        <h3 className='font-bold'>Max. speed:</h3>
+                        <p className={`${delaGothic.className} text-xl`}>32 km/h</p>
                     </div>
+                </div>
 
-                    <div className='flex w-full justify-between mt-4'>
-                        <div className='mr-3'>
-                            <h3 className='font-bold'>Medals:</h3>
-                            <p className={`${delaGothic.className}`}>16</p>
-                        </div>
-                        <div>
-                            <h3 className='font-bold'>Marathons:</h3>
-                            <p className={`${delaGothic.className}`}>7</p>
-                        </div>
+                <div className='flex w-full justify-between mt-4'>
+                    <div className='mr-3'>
+                        <h3 className='font-bold'>Max. distance:</h3>
+                        <p className={`${delaGothic.className} text-xl`}>15 km</p>
                     </div>
-
-                    <div className='flex w-full justify-between mt-4'>
-                        <div className='mr-3'>
-                            <h3 className='font-bold'>Max. speed:</h3>
-                            <p className={`${delaGothic.className} text-xl`}>32 km/h</p>
-                        </div>
-                    </div>
-
-                    <div className='flex w-full justify-between mt-4'>
-                        <div className='mr-3'>
-                            <h3 className='font-bold'>Max. distance:</h3>
-                            <p className={`${delaGothic.className} text-xl`}>15 km</p>
-                        </div>
-                    </div>
-
                 </div>
             </div>
 
             <div>
 
-                <h3 className='font-bold text-lg mt-4 mb-3'>My best video:</h3>
+                <h3 className='font-bold text-lg mt-8 mb-3'>My best video:</h3>
                 <video src="" controls className='w-full h-[6rem]'></video>
 
             </div>
--- src/components/sport-cards/SwimmingCard.tsx
+++ src/components/sport-cards/SwimmingCard.tsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import { NoTeamPhoto } from '../no-file-components/NoTeamPhoto';
-
 import { Dela_Gothic_One } from "@next/font/google";
 
 const delaGothic = Dela_Gothic_One({
@@ -15,39 +13,35 @@ export const SwimmingCard = () => {
 
             <div className='flex flex-col justify-between w-full items-center'>
 
-                <div>
-
-                    <div className='flex w-full justify-between mt-4'>
-                        <div className='mr-3'>
-                            <h3 className='font-bold'>Best Distance:</h3>
-                            <p className={`${delaGothic.className}`}>100 M</p>
-                        </div>
-                        <div>
-                            <h3 className='font-bold'>Time:</h3>
-                            <p className={`${delaGothic.className}`}>72 seconds</p>
-                        </div>
+                <div className='flex w-full justify-between mt-4'>
+                    <div className='mr-3'>
+                        <h3 className='font-bold'>Best Distance:</h3>
+                        <p className={`${delaGothic.className}`}>100 M</p>
                     </div>
-
-                    <div className='flex w-full justify-between mt-4'>
-                        <div className='mr-3'>
-                            <h3 className='font-bold'>Number of achievements:</h3>
-                            <p className={`${delaGothic.className}`}>53</p>
-                        </div>
+                    <div>
+                        <h3 className='font-bold'>Time:</h3>
+                        <p className={`${delaGothic.className}`}>72 seconds</p>
                     </div>
+                </div>
 
-                    <div className='flex w-full justify-between mt-4'>
-                        <div className='mr-3'>
-                            <h3 className='font-bold'>Major achievement:</h3>
-                            <p className={`${delaGothic.className} text-xl`}>Silver Medal</p>
-                        </div>
+                <div className='flex w-full justify-between mt-4'>
+                    <div className='mr-3'>
+                        <h3 className='font-bold'>Number of achievements:</h3>
+                        <p className={`${delaGothic.className}`}>53</p>
                     </div>
+                </div>
 
+                <div className='flex w-full justify-between mt-4'>
+                    <div className='mr-3'>
+                        <h3 className='font-bold'>Major achievement:</h3>
+                        <p className={`${delaGothic.className} text-xl`}>Silver Medal</p>
+                    </div>
                 </div>
             </div>
 
             <div>
 
-                <h3 className='font-bold text-lg mt-4 mb-3'>Best video of my career: </h3>
+                <h3 className='font-bold text-lg mt-8 mb-3'>Best video of my career: </h3>
                 <video src="" controls className='w-full h-[6rem]'></video>
 
             </div>
