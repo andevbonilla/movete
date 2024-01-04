155a8c6f0e907963e8fff0d48d8c6b14e18aa7ff
Andres Bonilla <andresbonilla1256@gmail.com>
2024-01-04 15:26:16 -0500

    soccer card first version finished

--- src/components/sport-cards/AthelticCard.tsx
+++ src/components/sport-cards/AthelticCard.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
-import { NoTeamPhoto } from '../no-file-components/NoTeamPhoto'
+import { NoTeamPhoto } from '../no-file-components/NoTeamPhoto';
+
+import { Dela_Gothic_One } from "@next/font/google";
+
+const delaGothic = Dela_Gothic_One({
+    weight: ["400"], // bold de la fuente
+    style: ["normal"],
+    subsets: ["latin"]
+});
 
 export const AthleticCard = () => {
     return (
@@ -14,28 +22,24 @@ export const AthleticCard = () => {
                 <div>
 
                     <div>
-                        <h2 className='font-bold text-xl'>Real Valencianos</h2>
+                        <h2 className={`${delaGothic.className} font-bold text-xl`}>Real Valencianos</h2>
                     </div>
 
                     <div className='flex w-full justify-between mt-4'>
-                        <div>
-                            <h3 className='font-bold'>Num:</h3>
-                            <p>3</p>
+                        <div className='mr-3'>
+                            <h3 className='font-bold'>Position:</h3>
+                            <p className={`${delaGothic.className}`}>Striker</p>
                         </div>
                         <div>
-                            <h3 className='font-bold'>Num:</h3>
-                            <p>3</p>
+                            <h3 className='font-bold'>Total Goals:</h3>
+                            <p className={`${delaGothic.className}`}>217</p>
                         </div>
                     </div>
 
                     <div className='flex w-full justify-between mt-4'>
-                        <div>
-                            <h3 className='font-bold'>Num:</h3>
-                            <p>3</p>
-                        </div>
-                        <div>
-                            <h3 className='font-bold'>Num:</h3>
-                            <p>3</p>
+                        <div className='mr-3'>
+                            <h3 className='font-bold'>Total Attendances:</h3>
+                            <p className={`${delaGothic.className}`}>117</p>
                         </div>
                     </div>
 
