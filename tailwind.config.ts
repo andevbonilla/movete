8067b6d539d14479fc8639d4fd817a714f83977b
Andres Bonilla <andresbonilla1256@gmail.com>
2024-01-12 01:05:15 -0500

    tailwind config changed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,25 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    screens: {
+      'um': '360px',
+      // => @media (min-width: 400px) { ... }
+
+      'sm': '650px',
+      // => @media (min-width: 650px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1100px',
+      // => @media (min-width: 1100px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: [],
 }
