50d3a364f7a400ada024f6721599d61d99627d4c
Andres Bonilla <andresbonilla1256@gmail.com>
2024-06-11 17:25:53 -0500

    updat link to claim your link

--- src/components/ClaimLink.tsx
+++ src/components/ClaimLink.tsx
@@ -18,7 +18,7 @@ export const ClaimLink = ({ placeholderText, buttonText }: any) => {
                 />
             </div>
             <a
-                href={`${process.env.NEXT_PUBLIC_DASHBOARD_LINK}/register/claim-your-link${(linkInput.length > 0) ? `?username=${linkInput}` : ""}`}
+                href={`${process.env.NEXT_PUBLIC_DASHBOARD_LINK}/claim-your-link${(linkInput.length > 0) ? `?username=${linkInput}` : ""}`}
                 className={`bg-[#00FF8F] mt-4 py-3 px-5 text-center w-full rounded font-extrabold text-lg`}
             >
                 {buttonText}
