53357c6e6c2985aa2b155845e0fc75c7ddbb0676
Andres Bonilla <andresbonilla1256@gmail.com>
2024-06-29 21:12:51 -0500

    small bugs solved

--- src/components/ClaimLink.tsx
+++ src/components/ClaimLink.tsx
@@ -8,7 +8,7 @@ export const ClaimLink = ({ placeholderText, buttonText }: any) => {
     return (
         <div className='w-full flex flex-col'>
             <div className='w-full flex items-center justify-start bg-white py-3 px-4 text-black rounded mt-4 text-lg'>
-                <span className='font-bold'>movete.bio/</span>
+                <span className='font-bold'>movete.me/</span>
                 <input
                     value={linkInput}
                     onChange={(e) => setlinkInput(e.target.value)}
